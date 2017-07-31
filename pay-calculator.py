7f8d8c0ee193f05cbfe46a7c658a87c6b0b5b0f1
Buck72 <mbuchholz@ufl.edu>
2017-07-31 09:48:54 -0400

    HotFix 0.2.2 code so it will calculate value.

--- pay-calculator.py
+++ pay-calculator.py
@@ -2,22 +2,24 @@
 # catching exceptions with try and except
 # updated with a function
 
-def computepay(hours, rate)
+def computepay(hours, rate):
     overtime = hours - 40
     if hours > 40:
         pay = (40 * rate) + (overtime * rate * 1.5)
+        pay = '${:,.2f}'.format(pay) #converts the float value to a currency dollars
         return pay
     else:
         pay = hours * rate
+        pay = '${:,.2f}'.format(pay)
         return pay
 
 try:
-    hours = raw_input('How many hours did you work?')
+    hours = raw_input('How many hours did you work? ')
     hours = float(hours)
-    rate = raw_input('How much did you get payed each hour?')
+    rate = raw_input('How much did you get payed each hour? ')
     rate = float(rate)
 except:
-    print 'Error, please enter numeric input'
+    print 'Error, please enter numeric input.'
 
 pay = computepay(hours, rate)
 
